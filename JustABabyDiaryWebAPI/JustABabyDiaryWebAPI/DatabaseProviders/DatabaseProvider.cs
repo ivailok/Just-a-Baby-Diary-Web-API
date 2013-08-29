97e2c4ec95c0cc457fb5b94ba3bbc849add62900
Lina Chalakova <chalakovalina@gmail.com>
2013-08-29 10:24:49 +0300

    created models for database communication, added empty controllers

---
+++ JustABabyDiaryWebAPI/JustABabyDiaryWebAPI/DatabaseProviders/DatabaseProvider.cs
@@ -0,0 +1,24 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace JustABabyDiaryWebAPI.DatabaseProviders
+{
+    public class DatabaseProvider
+    {
+        private const MongoClient mongoClient = new MongoClient(
+                "mongodb://appharbor_2c3c18a5-d2cb-4d0a-a8cd-a7015d56fbff:l52fm3f78ogrtiavsdcfoa8i78@ds041758.mongolab.com:41758/appharbor_2c3c18a5-d2cb-4d0a-a8cd-a7015d56fbff");
+
+        public static MongoDatabase GetMongoDatabase()
+        {
+            MongoServer mongoServer = mongoClient.GetServer();
+
+            MongoDatabase mongoDatabase = mongoServer.GetDatabase("appharbor_2c3c18a5-d2cb-4d0a-a8cd-a7015d56fbff");
+
+            return mongoDatabase;
+        }
+
+    }
+}
---
+++ JustABabyDiaryWebAPI/JustABabyDiaryWebAPI/Models/Gender.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace JustABabyDiaryWebAPI.Models
+{
+    public enum Gender
+    {
+        Female=0,
+        Male=0
+    }
+}
