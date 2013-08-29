3f0560a23a25f7ad587c4445c823e57db2188a01
Ivaylo Ivanov Kirilov <ivailokirilov93@gmail.com>
2013-08-29 12:15:58 +0300

    Register almost ready

--- JustABabyDiaryWebAPI/JustABabyDiaryWebAPI/DatabaseManipulators/UserDbManipulator.cs
+++ JustABabyDiaryWebAPI/JustABabyDiaryWebAPI/DatabaseManipulators/UserDbManipulator.cs
@@ -4,22 +4,33 @@ using System.Linq;
 using System.Web;
 using JustABabyDiaryWebAPI.Models;
 using MongoDB.Driver;
+using MongoDB.Driver.Linq;
 
 namespace JustABabyDiaryWebAPI.DatabaseManipulators
 {
     public class UserDbManipulator
     {
-        private MongoDatabase db;
+        private MongoCollection<User> usersCollecion;
 
         public UserDbManipulator()
         {
             DatabaseProviders.DatabaseProvider provider = new DatabaseProviders.DatabaseProvider();
-            this.db = provider.GetMongoDatabase();
+            MongoDatabase db = provider.GetMongoDatabase();
+            this.usersCollecion = db.GetCollection<User>("usersInfo");
         }
 
         public User Register(User user)
         {
-            
+            var foundUser = this.usersCollecion.AsQueryable().Single(u => u.Username == user.Username);
+
+            if (foundUser == null)
+            {
+                var result = this.usersCollecion.Insert<User>(user);
+            }
+            else
+            {
+                throw new ArgumentException("Username is already taken.");
+            }
         }
     }
 }
