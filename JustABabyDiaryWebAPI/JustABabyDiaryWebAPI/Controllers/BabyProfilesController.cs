f8d0062b0988e0e5d9fc7c1ac04d7407afbe9cab
Lina Chalakova <chalakovalina@gmail.com>
2013-08-29 14:48:19 +0300

    working on babyprofilecontroller

--- JustABabyDiaryWebAPI/JustABabyDiaryWebAPI/Controllers/BabyProfilesController.cs
+++ JustABabyDiaryWebAPI/JustABabyDiaryWebAPI/Controllers/BabyProfilesController.cs
@@ -5,10 +5,12 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using JustABabyDiaryWebAPI.Models.ControllerModels;
+using JustABabyDiaryWebAPI.Models;
 
 namespace JustABabyDiaryWebAPI.Controllers
 {
-    public class BabyProfilesController : ApiController
+    public class BabyProfilesController : BaseController
     {
         private MongoDatabase db;
 
@@ -16,6 +18,27 @@ namespace JustABabyDiaryWebAPI.Controllers
         {
             DatabaseProviders.DatabaseProvider provider = new DatabaseProviders.DatabaseProvider();
             this.db = provider.GetMongoDatabase();
+        }
+
+        public HttpResponseMessage PostBabyProfile(BabyProfileModel babyModel)
+        {
+            HttpResponseMessage responseMsg = this.PerformOperationAndHandleExceptions(
+                () =>
+                {
+                    BabyProfile profile = new BabyProfile
+                    {
+                        Name=babyModel.Name,
+                        BirthDay=babyModel.BirthDay,
+
+                    
+                    };
+
+                    var response = this.Request.CreateResponse(HttpStatusCode.Created);
+                    return response;
+                }
+            );
+
+            return responseMsg;
         }
     }
 }
