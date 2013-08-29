e993b26b90efdefbac6225b8ac46b1910dcd72be
Lina Chalakova <chalakovalina@gmail.com>
2013-08-29 21:23:04 +0300

    finished baby profile update

--- JustABabyDiaryWebAPI/JustABabyDiaryWebAPI/Controllers/BabyProfilesController.cs
+++ JustABabyDiaryWebAPI/JustABabyDiaryWebAPI/Controllers/BabyProfilesController.cs
@@ -73,8 +73,8 @@ namespace JustABabyDiaryWebAPI.Controllers
                () =>
                {
                    var usersWithSpecificSessionKey = from u in this.db.GetCollection<User>("usersInfo").AsQueryable()
-                                             where u.SessionKey == sessionKey
-                                             select u;
+                                                     where u.SessionKey == sessionKey
+                                                     select u;
                    User selectedUser = usersWithSpecificSessionKey.FirstOrDefault();
 
                    if (selectedUser == null)
@@ -84,21 +84,13 @@ namespace JustABabyDiaryWebAPI.Controllers
 
                    var babyCollection = this.db.GetCollection<BabyProfile>("user" + selectedUser.Id.ToString());
                    var babyProfilesWithSpecific = from p in babyCollection.AsQueryable()
-                                     where p.Name == name
-                                     select p;
+                                                  where p.Name == name
+                                                  select p;
 
                    var selectedBabyProfile = babyProfilesWithSpecific.FirstOrDefault();
 
-                   if (babyModel.Name != null)
-                   {
-                       var query = new QueryDocument { { "Name", selectedBabyProfile.Name } };
-                       var update = new UpdateDocument { { "$set", new BsonDocument("Name", babyModel.Name) } };
-                       babyCollection.Update(query, update);
-                   }
+                   ChangePropertiesOfBabyProfile(babyModel, babyCollection, selectedBabyProfile);
 
-                   //var query = new QueryDocument { { "Username", selectedUser.Username } };
-                   //var update = new UpdateDocument {{ "$set", new BsonDocument("SessionKey", "") }};
-                   //this.usersCollection.Update(query, update);
                    var response = this.Request.CreateResponse(HttpStatusCode.OK);
                    return response;
                }
@@ -107,6 +99,64 @@ namespace JustABabyDiaryWebAPI.Controllers
             return responseMsg;
         }
 
+        private void ChangePropertiesOfBabyProfile(BabyProfileModel babyModel,
+            MongoCollection babyCollection,BabyProfile selectedBabyProfile)
+        {
 
+            if (babyModel.Name != null)
+            {
+                var query = new QueryDocument { { "Name", selectedBabyProfile.Name } };
+                var update = new UpdateDocument { { "$set", new BsonDocument("Name", babyModel.Name) } };
+                babyCollection.Update(query, update);
+            }
+            if (babyModel.Mother != null)
+            {
+                var query = new QueryDocument { { "Mother", selectedBabyProfile.Mother } };
+                var update = new UpdateDocument { { "$set", new BsonDocument("Mother", babyModel.Mother) } };
+                babyCollection.Update(query, update);
+            }
+            if (babyModel.Father != null)
+            {
+                var query = new QueryDocument { { "Father", selectedBabyProfile.Father } };
+                var update = new UpdateDocument { { "$set", new BsonDocument("Father", babyModel.Father) } };
+                babyCollection.Update(query, update);
+            }
+            if (babyModel.BirthDay != null)
+            {
+                var query = new QueryDocument { { "BirthDay", selectedBabyProfile.BirthDay } };
+                var update = new UpdateDocument { { "$set", new BsonDocument("BirthDay", babyModel.BirthDay) } };
+                babyCollection.Update(query, update);
+            }
+            if (babyModel.BirthWeight != null)
+            {
+                var query = new QueryDocument { { "BirthWeight", selectedBabyProfile.BirthWeight } };
+                var update = new UpdateDocument { { "$set", new BsonDocument("BirthWeight", babyModel.BirthWeight) } };
+                babyCollection.Update(query, update);
+            }
+            if (babyModel.Gender != null)
+            {
+                var query = new QueryDocument { { "Gender", selectedBabyProfile.Gender } };
+                var update = new UpdateDocument { { "$set", new BsonDocument("Gender", babyModel.Gender) } };
+                babyCollection.Update(query, update);
+            }
+            if (babyModel.Height != null)
+            {
+                var query = new QueryDocument { { "Height", selectedBabyProfile.Height } };
+                var update = new UpdateDocument { { "$set", new BsonDocument("Height", babyModel.Height) } };
+                babyCollection.Update(query, update);
+            }
+            if (babyModel.PictureName != null)
+            {
+                var query = new QueryDocument { { "PictureName", selectedBabyProfile.PictureName } };
+                var update = new UpdateDocument { { "$set", new BsonDocument("PictureName", babyModel.PictureName) } };
+                babyCollection.Update(query, update);
+            }
+            if (babyModel.TownOfBirth != null)
+            {
+                var query = new QueryDocument { { "TownOfBirth", selectedBabyProfile.TownOfBirth } };
+                var update = new UpdateDocument { { "$set", new BsonDocument("TownOfBirth", babyModel.TownOfBirth) } };
+                babyCollection.Update(query, update);
+            }
+        }
     }
 }
