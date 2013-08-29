dc2a2fe371dee3a051ce1a826a673f6a2c2fcc63
Ivaylo Ivanov Kirilov <ivailokirilov93@gmail.com>
2013-08-29 10:42:37 +0300

    MongoDbProvider refactored

--- JustABabyDiaryWebAPI/JustABabyDiaryWebAPI/DatabaseProviders/DatabaseProvider.cs
+++ JustABabyDiaryWebAPI/JustABabyDiaryWebAPI/DatabaseProviders/DatabaseProvider.cs
@@ -8,16 +8,26 @@ namespace JustABabyDiaryWebAPI.DatabaseProviders
 {
     public class DatabaseProvider
     {
-        private const MongoClient mongoClient = new MongoClient(
+        private MongoDatabase mongoDatabase;
+
+        public DatabaseProvider()
+        {
+            this.InitializeDatabase();
+        }
+
+        private void InitializeDatabase()
+        {
+            MongoClient mongoClient = new MongoClient(
                 "mongodb://appharbor_2c3c18a5-d2cb-4d0a-a8cd-a7015d56fbff:l52fm3f78ogrtiavsdcfoa8i78@ds041758.mongolab.com:41758/appharbor_2c3c18a5-d2cb-4d0a-a8cd-a7015d56fbff");
 
-        public static MongoDatabase GetMongoDatabase()
-        {
             MongoServer mongoServer = mongoClient.GetServer();
 
-            MongoDatabase mongoDatabase = mongoServer.GetDatabase("appharbor_2c3c18a5-d2cb-4d0a-a8cd-a7015d56fbff");
+            this.mongoDatabase = mongoServer.GetDatabase("appharbor_2c3c18a5-d2cb-4d0a-a8cd-a7015d56fbff");
+        }
 
-            return mongoDatabase;
+        public MongoDatabase GetMongoDatabase()
+        {
+            return this.mongoDatabase;
         }
 
     }
