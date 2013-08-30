f3755cfe0e5d2eb4544d28fa0eeb151d1222dd5b
Lina Chalakova <chalakovalina@gmail.com>
2013-08-30 10:43:18 +0300

    Working on babyEvents controller
    added models for events

--- JustABabyDiaryWebAPI/JustABabyDiaryWebAPI/Controllers/BabyEventsController.cs
+++ JustABabyDiaryWebAPI/JustABabyDiaryWebAPI/Controllers/BabyEventsController.cs
@@ -1,13 +1,63 @@
-﻿using System;
+﻿using BloggingSystem.WebAPI.Attributes;
+using JustABabyDiaryWebAPI.Models;
+using JustABabyDiaryWebAPI.Models.ControllerModels;
+using MongoDB.Driver;
+using System;
+using MongoDB.Driver.Linq;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Http.ValueProviders;
 
 namespace JustABabyDiaryWebAPI.Controllers
 {
-    public class BabyEventsController : ApiController
+    public class BabyEventsController : BaseController
     {
+         private MongoDatabase db;
+
+         public BabyEventsController()
+        {
+            DatabaseProviders.DatabaseProvider provider = new DatabaseProviders.DatabaseProvider();
+            this.db = provider.GetMongoDatabase();
+        }
+
+        [HttpPost]
+        public HttpResponseMessage PostBabyEvent([FromBody]BabyEventModel babyEventModel,
+            [ValueProvider(typeof(HeaderValueProviderFactory<string>))]string sessionKey, string babyProfileId)
+        {
+            HttpResponseMessage responseMsg = this.PerformOperationAndHandleExceptions(
+                () =>
+                {
+                    var usersWithSpecificId = from u in this.db.GetCollection<User>("usersInfo").AsQueryable()
+                                              where u.SessionKey == sessionKey
+                                              select u;
+                    User selectedUser = usersWithSpecificId.FirstOrDefault();
+
+                    if (selectedUser == null)
+                    {
+                        throw new NullReferenceException("User is logged out or does not exist!");
+                    }
+
+                    BabyEvent babyEvent = new BabyEvent()
+                    {
+                        Title=babyEventModel.Title,
+                        Date=babyEventModel.Date,
+                        Description=babyEventModel.Description,
+                        PictureNames=babyEventModel.PictureNames
+                    };
+
+                    var collection = this.db.GetCollection("baby" + babyProfileId);
+                    collection.Insert<BabyEvent>(babyEvent);
+
+                    var response = this.Request.CreateResponse(HttpStatusCode.Created, babyEvent.Id.ToString());
+                    return response;
+                }
+            );
+
+            return responseMsg;
+        }
+
     }
 }
---
+++ JustABabyDiaryWebAPI/JustABabyDiaryWebAPI/Models/ControllerModels/BabyEventModel.cs
@@ -0,0 +1,19 @@
+﻿using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace JustABabyDiaryWebAPI.Models.ControllerModels
+{
+    public class BabyEventModel
+    {
+        public string Title { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public string Description { get; set; }
+
+        public ICollection<string> PictureNames { get; set; }
+    }
+}
