68f9acd3e0c3570306486b72ed7029ed95631ab7
Ivaylo Ivanov Kirilov <ivailokirilov93@gmail.com>
2013-08-30 10:06:58 +0300

    BabyGetProfileModel

---
+++ JustABabyDiaryWebAPI/JustABabyDiaryWebAPI/Models/ControllerModels/BabyGetProfileModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace JustABabyDiaryWebAPI.Models.ControllerModels
+{
+    public class BabyGetProfileModel
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTime BirthDay { get; set; }
+
+        public string Gender { get; set; }
+
+        public string Mother { get; set; }
+
+        public string Father { get; set; }
+
+        public string PictureName { get; set; }
+
+        public string TownOfBirth { get; set; }
+
+        public int BirthWeight { get; set; }
+
+        public int Height { get; set; }
+    }
+}
