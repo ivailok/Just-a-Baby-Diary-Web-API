9eadc17e779f6e65d91966c4e05b932ac8470e0b
Lina Chalakova <chalakovalina@gmail.com>
2013-09-05 13:24:50 +0300

    changed database

--- JustABabyDiaryWebAPI/JustABabyDiaryWebAPI/DatabaseProviders/DatabaseProvider.cs
+++ JustABabyDiaryWebAPI/JustABabyDiaryWebAPI/DatabaseProviders/DatabaseProvider.cs
@@ -17,13 +17,13 @@ namespace JustABabyDiaryWebAPI.DatabaseProviders
 
         private void InitializeDatabase()
         {
-            string primeConnectionString = "mongodb://appharbor_2c3c18a5-d2cb-4d0a-a8cd-a7015d56fbff:l52fm3f78ogrtiavsdcfoa8i78@ds041758.mongolab.com:41758/appharbor_2c3c18a5-d2cb-4d0a-a8cd-a7015d56fbff";
+            string primeConnectionString = "mongodb://appharbor_83641b3e-2d74-46aa-9a7c-2c8fe346e9bd:83vv6tk99o2a690rr31jig2j13@ds043168.mongolab.com:43168/appharbor_83641b3e-2d74-46aa-9a7c-2c8fe346e9bd";
             string testConnectionString = "mongodb://127.0.0.1/JustABabyDiary";
 
             MongoClient mongoClient = new MongoClient(primeConnectionString);
             MongoServer mongoServer = mongoClient.GetServer();
 
-            this.mongoDatabase = mongoServer.GetDatabase("appharbor_2c3c18a5-d2cb-4d0a-a8cd-a7015d56fbff");
+            this.mongoDatabase = mongoServer.GetDatabase("appharbor_83641b3e-2d74-46aa-9a7c-2c8fe346e9bd");
         }
 
         public MongoDatabase GetMongoDatabase()
